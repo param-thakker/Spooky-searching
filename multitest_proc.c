d892931160c683607e2187516bbff3870cd8fc96
Daniel Bezden <danvicbez@yahoo.com>
2019-11-11 21:51:13 -0500

    Add process search

--- multitest_proc.c
+++ multitest_proc.c
@@ -10,10 +10,24 @@ int search(int *array, int size, int value, int chunkSize) {
     for(i = 0; i < size; i += chunkSize) {
         pid_t pid = fork();
         if(pid == 0) {
-            printf("in child process, pid = %d\n", getpid());
-            exit(0);
+            printf("in child process, searching indices %d-%d\n", i, i + chunkSize - 1);
+            
+            int j;
+            for(j = i; j < i + chunkSize; j++) {
+				if(array[j] == value) {
+					printf("found value, returning %d\n", j);
+					exit(j);
+				}
+            }
+
+			printf("value not found, returning -1\n");
+            exit(-1);
         } else {
             printf("child PID: %d\n", pid);
+
+			int status;
+			wait(&status);
+			printf("search returned %d\n", WEXITSTATUS(status));
         }
     }
 }
